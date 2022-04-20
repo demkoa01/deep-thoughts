eba523cdaee53669e1e8240bdad7e9708f044cc7
Allison Demkovich <demkovich.allison@gmail.com>
2022-04-20 13:52:27 -0500

    add user authentication

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,8 @@ import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
-import Signup from './pages/Signup'
+import Signup from './pages/Signup';
+import { setContext } from '@apollo/client/link/context';
 
 import Home from './pages/Home';
 
@@ -15,8 +16,18 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const Header = () => {
+  const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
@@ -10,8 +25,19 @@ const Header = () => {
         </Link>
 
         <nav className="text-center">
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
+          {Auth.loggedIn() ? (
+            <>
+            <Link to ="/profile">Me</Link>
+            <a href="/" onClick={logout}>
+              Logout
+            </a>
+          </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </>
+        )}
         </nav>
       </div>
     </header>
